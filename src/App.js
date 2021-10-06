1cdd83525513932a10ae81b8a75881b7f6cf546c
Upasana108 <91844128+Upasana108@users.noreply.github.com>
2021-10-06 19:30:31 +0530

    Table and error message completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import ErrorBody from "./ErrorBody";
 import RepoTable from "./RepoTable";
 
 const App = (props) => {
+  const [user, setUser] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const [apiSuccess, setApiSuccess] = useState(false);
+  // const [error, setError] = useState(false);
+
+  const url = `https://api.github.com/users/${user}/repos`;
+  const fetchData = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setTableData(data);
+      if (res.ok) {
+        setApiSuccess(true);
+      } else {
+        setApiSuccess(false);
+      }
+      console.log("Data** ", res);
+    } catch (error) {
+      // setError(true);
+      console.log("Error** ", error);
+    }
+  };
+  // console.log("Inppur update ", error);
   return (
     <React.Fragment>
       <div className="mainBody">
         <div className="element-position">
           <span>Enter GitHub Username:</span>
-          <input type="text" className="user-input" />
+          <input
+            type="text"
+            className="user-input"
+            onChange={(event) => {
+              setUser(event.target.value);
+            }}
+          />
         </div>
         <div className="element-position button">
-          <input type="submit" onClick={() => {}} />
+          <input
+            type="submit"
+            onClick={(event) => {
+              event.preventDefault();
+              fetchData();
+            }}
+          />
         </div>
-        <ErrorBody userName="Upasana" />
+        {!apiSuccess && <ErrorBody userName={user} />}
       </div>
-      <RepoTable data="" />
+      {apiSuccess && tableData && tableData.length > 0 && (
+        <RepoTable data={tableData} />
+      )}
     </React.Fragment>
   );
 };
